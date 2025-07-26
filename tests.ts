edc5e36fea8a402ad70edac261f1d40d5a45be12
kyohei <kyohei103@outlook.jp>
2025-07-26 09:27:11 +0900

    質問

--- tests.ts
+++ tests.ts
@@ -1,12 +1,34 @@
 import { tsukuru } from "./tsukuru"
 
-nankaimo(101)
+const kaitou = question("kaitou (1/2)", 1, 2)
+const mozi = question("mozi (1/2/3)", 2, 3)
+const nagasa = question("nagasa", 10, Infinity)
+const kaisuu = question("kaisuu", 10, Infinity)
 
-function nankaimo(kai: number) {
+nankaimo(kaisuu, mozi, nagasa, kaitou)
+
+function question(text: string, de: number, max: number) {
+  let suji: number = 0
+  while(isNaN(suji) || suji === 0 || suji < 0 || suji > max) {
+    const kotae = prompt(text, de.toString())
+    suji = Number(kotae)
+    if (!kotae) {
+      suji = de
+    }
+  }
+  return suji
+}
+
+function nankaimo(kai: number, pa: number, na: number, kk: number) {
   let count = 0
   while (kai > count) {
-    const dekita = tsukuru(1, 10)
-    const log = no(kai, count) + ": " + dekita + " & " + dekita.length
+    const dekita = tsukuru(pa, na)
+    let log = ""
+    if (kk == 1) {
+      log = no(kai, count) + ": " + dekita + " & " + dekita.length
+    } else {
+      log = dekita
+    }
     console.log(log)
     count += 1
   }
